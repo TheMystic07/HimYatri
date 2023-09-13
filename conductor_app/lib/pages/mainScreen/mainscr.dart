3fb073deb2467fc44a40746874e608c6547c87e2
Mystic <gushabad90@gmail.com>
2023-09-14 03:06:30 +0530

    Removed Unused Import Statements

--- conductor_app/lib/pages/mainScreen/mainscr.dart
+++ conductor_app/lib/pages/mainScreen/mainscr.dart
@@ -1,3 +1,7 @@
+import 'package:conductor_app/pages/navScreens/home.dart';
+import 'package:conductor_app/pages/navScreens/profile.dart';
+import 'package:conductor_app/pages/navScreens/seats.dart';
+import 'package:conductor_app/pages/navScreens/set_route.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -29,13 +33,33 @@ class _MainScreenState extends State<MainScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(
-        //yeh Navigation bar ak code  hai and  aur mai jaa rha soone
+        body: TabBarView(
+          //yeh Navigation bar ak code  hai and  aur mai jaa rha soone
 
-        physics: const NeverScrollableScrollPhysics(),
-        controller: tabController,
-        children: const [],
-      ),
-    );
+          physics: const NeverScrollableScrollPhysics(),
+          controller: tabController,
+          children: const [Home(), Profile(), Seats(), SetRoute()],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), label: "Profile"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.chair_alt_rounded), label: "Seats"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.map_rounded), label: "Route"),
+          ],
+          unselectedItemColor: Colors.white54,
+          selectedItemColor: Colors.amber,
+          backgroundColor: Colors.black,
+          type: BottomNavigationBarType.fixed,
+          selectedLabelStyle: const TextStyle(
+            fontSize: 10,
+          ),
+          showUnselectedLabels: true,
+          currentIndex: selectedIndex,
+          onTap: onClicked,
+        ));
   }
 }
