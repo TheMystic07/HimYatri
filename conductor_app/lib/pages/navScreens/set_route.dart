a769210450e59919e1acc01a512df4f43e4b63f0
Mystic <gushabad90@gmail.com>
2023-09-14 03:01:29 +0530

    Added Set/Update Route(BUS)

---
+++ conductor_app/lib/pages/navScreens/set_route.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SetRoute extends StatefulWidget {
+  const SetRoute({super.key});
+
+  @override
+  State<SetRoute> createState() => _SetRouteState();
+}
+
+class _SetRouteState extends State<SetRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("janna kaha hai "),
+    );
+  }
+}
